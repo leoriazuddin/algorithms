6a160ff6dc4d1051cfb1b476aec748725178f8fb
Riaz Uddin <riaz.uddin@teradata.com>
2020-01-23 08:48:44 -0800

    fixed calculator

--- src/main/java/strings/BasicCalculator.java
+++ src/main/java/strings/BasicCalculator.java
@@ -2,13 +2,16 @@ package strings;
 
 /**
  * https://leetcode.com/problems/basic-calculator-ii/description/
+ *
+ * Build char array
+ *
  */
 public class BasicCalculator {
 
     public int calc(String in) {
 
-        int md = -1; // * = 0, / = 1
-        int sign = -1; // 1 is +, -1 is -
+        int mul_div = -1; // * = 0, / = 1
+        int sign = 1; // 1 is +, -1 is -
         int prev = 0;
         int result = 0;
 
@@ -18,34 +21,38 @@ public class BasicCalculator {
             char c = chars[i];
             int num = c - '0';
             if (Character.isDigit(c)) {
-                while (++i < chars.length && Character.isDigit(chars[++i])) {
-                    num = num * 10 + chars[i] - '0';
+                while ((i+1) < chars.length && Character.isDigit(chars[i+1])) {
+                    num = num * 10 + chars[++i] - '0';
                 }
-                i--;
 
-                if (md == 0) {
+                if (mul_div == 0) {
                     prev = prev * num;
-                    md = -1;
-                } else if (md == 1) {
+                    mul_div = -1;
+                } else if (mul_div == 1) {
                     prev = prev / num;
-                    md = -1;
+                    mul_div = -1;
                 } else {
                     prev = num;
                 }
             } else if (c == '+') {
-                result = result + prev * sign;
+                result += prev * sign;
                 sign = 1;
             } else if (c == '-') {
-                result = result + prev * sign;
+                result += prev * sign;
                 sign = -1;
             } else if (c == '*') {
-                md = 0;
+                mul_div = 0;
             } else if (c == '/') {
-                md = 1;
+                mul_div = 1;
             }
         }
 
-        result = result + sign * prev;
+        result += prev * sign;
         return result;
+    }
+
+    public static void main(String[] args) {
+        BasicCalculator c = new BasicCalculator();
+        System.out.println(c.calc("3*2*2"));
     }
 }
